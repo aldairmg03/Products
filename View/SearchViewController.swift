2c0f12e1f701baf3264bcce710bea180331b26ce
aldairmg <amartinezg7455@gmail.com>
2022-11-24 13:36:54 -0600

    Added UINavigationController and refactor.

---
+++ View/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  Products
+//
+//  Created by Aldair Martínez on 24/11/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
