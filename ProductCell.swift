293bbd3e49ed56b46504c434a99b7a3808753cb7
aldairmg <amartinezg7455@gmail.com>
2022-11-24 12:44:22 -0600

    Added ProductCell for show product.

--- ProductCell.swift
+++ ProductCell.swift
@@ -7,17 +7,92 @@
 
 import UIKit
 
-class ProductCell: UITableViewCell {
+class ProductCell: UITableViewCell, ReusableView {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private lazy var thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 8.0
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 3
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        return label
+    }()
+
+    private lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .darkGray
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        //stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupCell()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupCell()
     }
 
+}
+
+private extension ProductCell {
+    func setupCell() {
+        backgroundColor = .clear
+        selectionStyle = .none
+        addViews()
+        setConstraints()
+    }
+    
+    func addViews() {
+        contentView.addSubview(thumbnailImageView)
+        contentView.addSubview(stackView)
+        stackView.addArrangedSubview(titleLabel)
+        stackView.addArrangedSubview(priceLabel)
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            thumbnailImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            thumbnailImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            thumbnailImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 8)
+        ])
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            stackView.leadingAnchor.constraint(equalTo: thumbnailImageView.trailingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 8)
+        ])
+        
+    }
+    
+}
+
+extension ProductCell {
+    
+    func configure(item: Items) {
+        titleLabel.text = item.name
+        priceLabel.text = "$\(item.price)"
+    }
+    
 }
